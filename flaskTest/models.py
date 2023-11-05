1566576ecdd615a99beb0f2213e079a96900629d
RinKonri <119094787+RinKonri@users.noreply.github.com>
2023-11-05 21:52:15 +0600

    mend
    edited gitignore, added /users get endpoint

--- flaskTest/models.py
+++ flaskTest/models.py
@@ -1,10 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
-# Initialize SQLAlchemy
 db = SQLAlchemy()
 
-# Define a User model class for the database
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(60), unique=True, nullable=False)
